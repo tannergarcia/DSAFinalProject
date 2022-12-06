b4cab59afd507ee9395da3d3534636433c46cd49
tannergarcia <92037724+tannergarcia@users.noreply.github.com>
2022-12-06 10:08:01 -0500

    cleanup, cmake

--- src/shellsort.h
+++ src/shellsort.h
@@ -9,7 +9,6 @@
 
 using namespace std;
 
-//comment this later
 long long int shellSort(std::vector<std::pair<std::string, int>> &data);
 
 long long int shellSort(std::vector<std::pair<int, int>> &data);
